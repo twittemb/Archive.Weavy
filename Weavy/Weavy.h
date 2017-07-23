29a3ca059f211b31a1fb724e8b8461de1c9863b3
Thibault Wittemberg <thibaultwittemberg@WarpBookPro.local>
2017-07-23 19:56:53 -0400

    project: bootstrap XCode project
    This commit bootstraps XCode project as a CocoaTouch framework

---
+++ Weavy/Weavy.h
@@ -0,0 +1,19 @@
+//
+//  Weavy.h
+//  Weavy
+//
+//  Created by Thibault Wittemberg on 17-07-23.
+//  Copyright © 2017 Warp Factor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Weavy.
+FOUNDATION_EXPORT double WeavyVersionNumber;
+
+//! Project version string for Weavy.
+FOUNDATION_EXPORT const unsigned char WeavyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Weavy/PublicHeader.h>
+
+
